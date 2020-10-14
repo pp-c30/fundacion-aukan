a4087f55c413e74a7827c85527be29c725f05356
Guille-B <guillebau2012@gmail.com>
2020-10-14 12:15:40 -0300

    p1

--- backend/build/index.js
+++ backend/build/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('probando');
+Object.defineProperty(exports, "__esModule", { value: true });
+const server_1 = require("./server");
+function principal() {
+    //se crea instancia de la clase
+    const servidor = new server_1.server();
+    // se le da arranque al servidor
+    servidor.listen();
+}
+//se ejecuta la funcion
+principal();
--- backend/build/server.js
+++ backend/build/server.js
@@ -1,1 +1,42 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.server = void 0;
+const express_1 = __importDefault(require("express"));
+class server {
+    constructor() {
+        this.app = express_1.default();
+        this.configuracion();
+        this.middleware();
+        this.routes();
+    }
+    configuracion() {
+        //se configura el puerto que se utilizara para correr el servidor
+        this.app.set('port', process.env.port || 3000);
+    }
+    //
+    routes() {
+    }
+    middleware() {
+        this.app.use(express_1.default.json());
+    }
+    //este metodo se encarga de correr el servidor
+    listen() {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield this.app.listen(this.app.get('port'));
+            console.log('Servidor corriendo en el puerto 3000');
+        });
+    }
+}
+exports.server = server;
